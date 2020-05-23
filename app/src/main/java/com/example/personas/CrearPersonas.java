435bcbc401f2f27ea4287c74c905e334edff3205
Jose Bandera <jbandera@Defensoria.gov.co>
2020-05-23 17:01:50 -0500

    Agregado string

--- app/src/main/java/com/example/personas/CrearPersonas.java
+++ app/src/main/java/com/example/personas/CrearPersonas.java
@@ -3,12 +3,55 @@ package com.example.personas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CrearPersonas extends AppCompatActivity {
+    private EditText cedula, nombre, apellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_personas);
+        cedula = findViewById(R.id.txtCedula);
+        nombre = findViewById(R.id.txtNombre);
+        apellido = findViewById(R.id.txtApellido);
     }
-}
+        public void guardar(View v){
+            String ced, nom, apell;
+            Persona persona;
+            if (validar()){
+                ced=cedula.getText().toString();
+                nom=nombre.getText().toString();
+                apell=apellido.getText().toString();
+                persona=new Persona(ced, nom, apell);
+                persona.guardar();
+
+            }
+        }
+        public boolean validar(){
+            String error_cedula, error_nombre, error_apellido;
+            error_cedula=getResources().getString(R.string.error_cedula);
+            error_nombre=getResources().getString(R.string.error_nombre);
+            error_apellido=getResources().getString(R.string.error_apellido);
+            if(cedula.getText().toString().isEmpty()){
+                cedula.setError("Digite la cédula de la persona");
+                return false;
+            }else if(nombre.getText().toString().isEmpty()){
+                nombre.setError("Digite el nombre de la persona");
+                return false;
+            }else if(apellido.getText().toString().isEmpty()){
+                apellido.setError("Digite el apellido de la persona");
+                return false;
+            }
+            return true;
+        }
+
+        public void limpiar(View v){
+            cedula.setText("");
+            nombre.setText("");
+            apellido.setText("");
+            cedula.requestFocus();
+        }
+    }
+
--- app/src/main/java/com/example/personas/Datos.java
+++ app/src/main/java/com/example/personas/Datos.java
@@ -1,4 +1,14 @@
 package com.example.personas;
 
+import java.util.ArrayList;
+
 public class Datos {
+    private static ArrayList<Persona> personas = new ArrayList();
+
+    public static void guardar(Persona p){
+        personas.add(p);
+    }
+    public static ArrayList<Persona> obtener(){
+        return personas;
+    }
 }
